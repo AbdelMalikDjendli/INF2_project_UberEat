72a6ece613f702b914618e6f39ac8e98cf039fe0
samosaaa <samra_adl@outlook.fr>
2023-12-13 13:52:39 +0100

    new branch

--- uber-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/Menu.java
+++ uber-commons/src/main/java/fr/pantheonsorbonne/ufr27/miage/dto/Menu.java
@@ -1,49 +1,5 @@
 package fr.pantheonsorbonne.ufr27.miage.dto;
 
-public class Menu {
-        private Long id;
-        private String name;
-        private String description;
-        private Double price;
-        private String darkKitchen;
+public record Menu(String name, Double price) {
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getDarkKitchen() {
-        return darkKitchen;
-    }
-
-    public void setDarkKitchen(String darkKitchen) {
-        this.darkKitchen = darkKitchen;
-    }
 }
--- uber-ubereat/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/Menu.java
+++ uber-ubereat/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/Menu.java
@@ -19,33 +19,4 @@ public class Menu {
     public Menu() {
     }
 
-    public Menu(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
